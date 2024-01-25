6b2679e55e433992fa755ef8d2841b3c50121563
Immanuel Maina <imaina673@gmail.com>
2024-01-25 08:28:20 +0300

    added routes

---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,10 @@
+const SignIn = () => {
+    return (
+        <div className='sign-in'>
+            <h1>I already have an account</h1>
+            <span>Sign in with your email and password</span>
+        </div>
+    )
+}
+
+export default SignIn;
